21333d7d4b2019c948d06b82b284df64b1b1ea49
cbonaco1 <christian.bonacore@gmail.com>
2016-02-18 20:30:46 -0500

    Added icons next to GOLD rotation titles which rotate, added hover effect for portfolio screenshots

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
+
+  //Display description of a GOLD rotation when
+  //hovering over its title
   $(".gold-rotations-title").hover(function(){
       $(this).next(".rotation-description").toggle(300);
+      $(this).prev().toggleClass("icon-rotate-90", 300);
   });
+
+  
 });
