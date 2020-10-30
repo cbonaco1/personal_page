1b2fadd8f81cde40c6203fcb14980e0fe9fa1007
cbonaco1 <christian.bonacore@gmail.com>
2020-10-30 15:57:54 -0400

    add About section

--- pages/index.js
+++ pages/index.js
@@ -1,53 +1,31 @@
 import Head from 'next/head'
+import Image from "next/image";
+
+import About from "../sections/About";
+
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Christian Bonacore</title>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic&display=swap" type="text/css"/>
+        <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Raleway:400,300,700&display=swap" type="text/css"/>
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <h1 className={styles.title}>Christian Bonacore</h1>
+        <p>Software Engineer</p>
+        <Image
+          src="/img/me.jpg"
+          alt="Christian Bonacore"
+          height={200}
+          width={200}
+          className={styles.me}
+        />
+        <About />
       </main>
 
       <footer className={styles.footer}>
---
+++ sections/About.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Section from "./Section";
+
+const About = () => {
+  return (
+    <Section>
+      <h3>About</h3>
+      <p>
+        I am a NYC-based software engineer passionate about building products that help people. I take pride in putting great detail into my work, and focusing on giving the user the best experience possible. Outside of coding, I enjoy sports, the beach, and a good cup of coffee. Read on to find out more about me and my story.
+      </p>
+    </Section>
+  )
+}
+
+export default About;
