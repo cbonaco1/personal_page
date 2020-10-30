2e56be10a9e39fe3722a019f86842cf87fc5c6b6
cbonaco1 <christian.bonacore@gmail.com>
2020-10-30 16:27:01 -0400

    add module path alias

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,9 @@
 import Head from 'next/head'
-import Image from "next/image";
+import Image from 'next/image'
 
-import About from "../sections/About";
+import About from 'sections/About'
 
-import styles from '../styles/Home.module.css'
+import styles from 'styles/Home.module.css'
 
 export default function Home() {
   return (
