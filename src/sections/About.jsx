d9c16d5072c634b0213397a01544f5682d2379df
cbonaco1 <christian.bonacore@gmail.com>
2021-02-07 13:58:41 -0500

    update copy

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -6,7 +6,10 @@ const About = () => {
     <Section>
       <h3>About</h3>
       <p>
-        I am a NYC-based software engineer passionate about building products that help people. I take pride in putting great detail into my work, and focusing on giving the user the best experience possible. Outside of coding, I enjoy sports, the beach, and a good cup of coffee. Read on to find out more about me and my story.
+        I am a NYC-based software engineer passionate about building products that help people. 
+        I take pride in putting great detail into my work, and focusing on giving the user the best experience possible. 
+        Outside of coding, I enjoy sports, the beach, and a good cup of coffee. 
+        Read on to find out more about me and my story.
       </p>
     </Section>
   )
