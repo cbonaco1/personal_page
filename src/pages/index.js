31e9235f01c308ab2729373cbe8e79c84660d62b
cbonaco1 <christian.bonacore@gmail.com>
2021-02-15 15:56:13 -0500

    add container, remove About

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,6 @@
 import Head from 'next/head'
 
 import Home from 'sections/Home'
-import About from 'sections/About'
 import Work from 'sections/Work'
 import Skills from 'sections/Skills'
 import Education from 'sections/Education'
@@ -17,7 +16,6 @@ export default function Index() {
       </Head>
       <main>
         <Home />
-        <About />
         <Work />
         <Skills />
         <Education />
--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -6,32 +6,33 @@ const Home = () => {
   return (
     <div className="hero is-fullheight">
       <div className="hero-body">
-        <div className="columns">
-          <div className="column">
-            <h1 className="title has-text-centered">
-              Hi,<br />
-              I'm Christian Bonacore
-            </h1>
-            <p className="subtitle has-text-centered">Software Engineer</p>
-            <p>
-              Did he just throw my cat out of the window? 
-              God help us, we're in the hands of engineers. 
-              Just my luck, no ice. Hey, take a look at the earthlings. 
-              Goodbye! Remind me to thank John for a lovely weekend. 
-              Forget the fat lady! You're obsessed with the fat lady!
-            </p>
-            <div className="buttons are-medium">
-              <button className="button is-primary">Contact Me</button>
-              <button className="button">Learn About Me</button>
+        <div className="container">
+          <div className="columns">
+            <div className="column">
+              <h1 className="title has-text-centered">
+                Hi,<br />
+                I'm Christian Bonacore
+              </h1>
+              <p className="subtitle has-text-centered">Software Engineer</p>
+              <p>
+              I am a NYC-based software engineer passionate about building products that help people. 
+              I take pride in putting great detail into my work, and focusing on giving the user the best experience possible. 
+              Outside of coding, I enjoy quoting Seinfeld, a good cup of coffee, or quoting Seinfeld over a good cup of coffee.
+              Read on to find out more about me and my story.
+              </p>
+              <div className="buttons are-medium">
+                <button className="button is-primary">Contact Me</button>
+                <button className="button">Learn About Me</button>
+              </div>
             </div>
-          </div>
-          <div className="column">
-            {/* TODO - use Next img here instead */}
-            <img
-              src="/img/me.jpg"
-              alt="Christian Bonacore"
-              className={styles.me}
-            />
+            <div className="column">
+              {/* TODO - use Next img here instead */}
+              <img
+                src="/img/me.jpg"
+                alt="Christian Bonacore"
+                className={styles.me}
+              />
+            </div>
           </div>
         </div>
       </div>
