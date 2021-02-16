8a54844b7f4dec0697e31bff27527f1fb09a3623
cbonaco1 <christian.bonacore@gmail.com>
2021-02-15 21:57:27 -0500

    typography and btn changes on hero

--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -7,22 +7,22 @@ const Home = () => {
     <div className="hero is-fullheight">
       <div className="hero-body">
         <div className="container">
-          <div className="columns">
+          <div className="columns is-vcentered">
             <div className="column">
-              <h1 className="title has-text-centered">
+              <h1 className="title">
                 Hi,<br />
                 I'm Christian Bonacore
               </h1>
-              <p className="subtitle has-text-centered">Software Engineer</p>
+              <p className="subtitle">Software Engineer</p>
               <p>
               I am a NYC-based software engineer passionate about building products that help people. 
               I take pride in putting great detail into my work, and focusing on giving the user the best experience possible. 
               Outside of coding, I enjoy quoting Seinfeld, a good cup of coffee, or quoting Seinfeld over a good cup of coffee.
               Read on to find out more about me and my story.
               </p>
-              <div className="buttons are-medium">
-                <button className="button is-primary">Contact Me</button>
-                <button className="button">Learn About Me</button>
+              <div className={`displayFlex ${styles.buttonGroup}`}>
+                <button className="button is-rounded is-primary">Contact Me</button>
+                <button className="button is-rounded">Learn About Me</button>
               </div>
             </div>
             <div className="column">
