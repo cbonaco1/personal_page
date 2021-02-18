40679cadad43735c051895018de0f4bc2018101f
cbonaco1 <christian.bonacore@gmail.com>
2021-02-17 21:37:28 -0500

    add boxes for Skills

--- src/sections/Section.jsx
+++ src/sections/Section.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-const Section = ({ children }) => {
+const Section = ({ children, className }) => {
   return (
-    <section className="section">
+    <section className={`section ${className ? className : ''}`}>
       <div className="container is-max-desktop" style={{border: '1px solid black'}}>
         { children }
       </div>
--- src/sections/Skills.jsx
+++ src/sections/Skills.jsx
@@ -5,50 +5,49 @@ import styles from "styles/Skills.module.scss";
 
 const Skills = () => {
   return (
-    <Section>
-      <div className={`displayFlex ${styles.container}`}>
-        <div className={`${styles.left}`}>
-          <h2>Skills</h2>
-          <p>
-            Checkmate... God help us, we're in the hands of engineers. 
-            They're using our own satellites against us. 
-            And the clock is ticking. 
-            Yeah, but your scientists were so preoccupied with whether or not they could, 
-            they didn't stop to think if they should. Remind me to thank John for a lovely weekend.
-          </p>
-        </div>
-        <div className={`${styles.right}`}>
-          <ul>
-            <li>
-              <h4>Develop</h4>
-              <p>Developer by trade, here are some of my skills:</p>
-              <ul>
-                <li>React</li>
-                <li>NodeJS</li>
-                <li>Express</li>
-                <li>Heroku</li>
-                <li>NextJS</li>
-              </ul>
-            </li>
-            <li>
-              <h4>Design</h4>
-              <ul>
-                <li>Responsive & Mobile-Friendly Design</li>
-                <li>Figma</li>
-                <li>Border-line obsession with UI/UX</li>
-                <li>Accessibility</li>
-              </ul>
-            </li>
-            <li>
-              <h4>Tools</h4>
-              <ul>
-                <li>Google Analytics</li>
-                <li>Google Lightouse</li>
-                <li>SEO</li>
-              </ul>
-            </li>
-          </ul>
-        </div>
+    <Section className="skills-section">
+      <div className={`${styles.left}`}>
+        <h2>Skills</h2>
+        <p>
+          Checkmate... God help us, we're in the hands of engineers. 
+          They're using our own satellites against us. 
+          And the clock is ticking. 
+          Yeah, but your scientists were so preoccupied with whether or not they could, 
+          they didn't stop to think if they should. Remind me to thank John for a lovely weekend.
+        </p>
+        <p>Developer by trade, here are some of my skills:</p>
+      </div>
+      <div>
+        <ul className="columns">
+          <li className="column box">
+            <h5 className="is-capitalized">develop</h5>
+            <ul>
+              <li>React</li>
+              <li>NodeJS</li>
+              <li>Express</li>
+              <li>Heroku</li>
+              <li>NextJS</li>
+            </ul>
+          </li>
+          <li className="column box">
+            <h5 className="is-capitalized">design</h5>
+            <ul>
+              <li>Responsive & Mobile-Friendly Design</li>
+              <li>Figma</li>
+              <li>Border-line obsession with UI/UX</li>
+              <li>Accessibility</li>
+            </ul>
+          </li>
+          <li className="column box">
+            <h5 className="is-capitalized">tools</h5>
+            <ul>
+              <li>SEO</li>
+              <li>Content Security Policy (CSP)</li>
+              <li>Google Analytics</li>
+              <li>Google Lightouse</li>
+            </ul>
+          </li>
+        </ul>
       </div>
     </Section>
   )
