fa54aa89e70baeb38d9166cce6bd86c965027764
cbonaco1 <christian.bonacore@gmail.com>
2021-02-18 21:47:49 -0500

    typography tweak

--- src/components/Pager.jsx
+++ src/components/Pager.jsx
@@ -20,7 +20,7 @@ const Pager = () => {
             />
           </div>
         </div>
-        <h4>Senior Software Engineer, Web{' '}<i>March 2018 - present</i></h4>
+        <p>Senior Software Engineer, Web{' '}<i>March 2018 - present</i></p>
         <p>This thing comes fully loaded. AM/FM radio, reclining bucket seats, and... power windows. You know what? It is beets. I've crashed into a beet truck. Just my luck, no ice. Hey, you know how I'm, like, always trying to save the planet? Here's my chance.</p>
       </div>
       <div className="column is-narrow">
