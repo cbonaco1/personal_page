93aa46ce3a87a7208b34f1d7c3f23ddeec466ec2
cbonaco1 <christian.bonacore@gmail.com>
2021-02-23 21:36:54 -0500

    add placeholder img section under hero

--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -4,36 +4,41 @@ import styles from 'styles/Home.module.scss'
 
 const Home = () => {
   return (
-    <div className={`hero is-fullheight ${styles.hero}`}>
-      <div className={`hero-body ${styles.heroBody}`}>
-        <div className="container">
-          <div className="columns is-vcentered ic-centered is-mobile">
-            <div className="column">
-              <h1 className="title">
-                Hi,<br />
-                I'm Christian Bonacore
-              </h1>
-              <p className="subtitle">Software Engineer</p>
-              <p>
-              I am a NYC-based software engineer passionate about building products that help people. 
-              I take pride in putting great detail into my work, and focusing on giving the user the best experience possible. 
-              Outside of coding, I enjoy quoting Seinfeld, a good cup of coffee, or quoting Seinfeld over a good cup of coffee.
-              Read on to find out more about me and my story.
-              </p>
-              <div className={`displayFlex ${styles.buttonGroup}`}>
-                <button className="button is-rounded is-primary">Contact Me</button>
-                <button className="button is-rounded">Learn About Me</button>
+    <div>
+      <div className={`hero is-fullheight ${styles.hero}`}>
+        <div className={`hero-body ${styles.heroBody}`}>
+          <div className="container">
+            <div className="columns is-vcentered ic-centered is-mobile">
+              <div className="column">
+                <h1 className="title">
+                  Hi,<br />
+                  I'm Christian Bonacore
+                </h1>
+                <p className="subtitle">Software Engineer</p>
+                <p>
+                I am a NYC-based software engineer passionate about building products that help people. 
+                I take pride in putting great detail into my work, and focusing on giving the user the best experience possible. 
+                Outside of coding, I enjoy quoting Seinfeld, a good cup of coffee, or quoting Seinfeld over a good cup of coffee.
+                Read on to find out more about me and my story.
+                </p>
+                <div className={`displayFlex ${styles.buttonGroup}`}>
+                  <button className="button is-rounded is-primary">Contact Me</button>
+                  <button className="button is-rounded">Learn About Me</button>
+                </div>
               </div>
-            </div>
-            <div className={`column ${styles.rightColumn}`}>
-              <img
-                src="/img/me.jpg"
-                alt="Christian Bonacore"
-                className={styles.me}
-              />
+              <div className={`column ${styles.rightColumn}`}>
+                <img
+                  src="/img/me.jpg"
+                  alt="Christian Bonacore"
+                  className={styles.me}
+                />
+              </div>
             </div>
           </div>
         </div>
+      </div>
+      <div className={`displayFlex ${styles.someImage}`}>
+        <p>some image goes here</p>
       </div>
     </div>
   )
--- src/sections/Section.jsx
+++ src/sections/Section.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Section = ({ children, className, containerClassName }) => {
   return (
     <section className={`section ${className ? className : ''}`}>
-      <div className={`container is-max-desktop ${containerClassName ? containerClassName : ''}`} style={{border: '1px solid black'}}>
+      <div className={`container is-max-desktop ${containerClassName ? containerClassName : ''}`}>
         { children }
       </div>
     </section>
