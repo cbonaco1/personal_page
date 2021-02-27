a22fad011015675ee6989e2bd0a5f6d1e176b403
cbonaco1 <christian.bonacore@gmail.com>
2021-02-27 11:13:54 -0500

    load fonts over https

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,8 +13,8 @@ export default function Index() {
       <Head>
         <title>Christian Bonacore</title>
         <link rel="icon" href="/favicon.ico" />
-        <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic&display=swap" type="text/css"/>
-        <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Raleway:400,300,700&display=swap" type="text/css"/>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic&display=swap" type="text/css"/>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway:400,300,700&display=swap" type="text/css"/>
       </Head>
       <main>
         <IconContext.Provider value={{ size: '1.5em' }}>
